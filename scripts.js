68ba30584cd553fec5d53042188fb2226f28b137
marcosaurelio36 <marcos.aureliodepadua@gmail.com>
2023-03-20 07:52:38 -0300

    subindo projeto para github

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function trocaCor(cor){
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+
+function trocaImagem(imagem){
+    let imgIphone = document.querySelector(".iPhone")
+    imgIphone.src = imagem
+}
